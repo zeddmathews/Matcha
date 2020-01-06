2b76024cf87cd6f170ed3cde9db27c00bb6ac320
Zain MATHEWS <zmathews@c5r11s5.wethinkcode.co.za>
2020-01-06 16:49:12 +0200

    modules

--- root.js
+++ root.js
@@ -3,25 +3,30 @@
 // import { readFile } from 'fs';
 
 const http = require('http');
+const path = require('path');
 const url = require('url');
-const files = require('fs');
+const fs = require('fs');
 
-http.createServer((req, res) => {
-	console.log('oi');
-	var q = url.parse(req.url, true);
-	var fileName = "." + q.pathname;
-	files.readFile(fileName, (err, data) => {
-		if (err) {
-			res.writeHead(404, {'Content-Type': 'text/html'});
-			console.log('Fail');
-			return res.end("404 Not Found");
-		}
-		res.writeHead(200, {'Content-Type' : 'text/html'});
-		res.write(data);
-		console.log('Success');
-		return res.end();
-	});
-}).listen(5000, () => console.log('Yes?'));
+const server = http.createServer((req, res) => {
+	
+})
+
+// http.createServer((req, res) => {
+// 	console.log('oi');
+// 	var q = url.parse(req.url, true);
+// 	var fileName = "." + q.pathname;
+// 	fs.readFile(fileName, (err, data) => {
+// 		if (err) {
+// 			res.writeHead(404, {'Content-Type': 'text/html'});
+// 			console.log('Fail');
+// 			return res.end("404 Not Found");
+// 		}
+// 		res.writeHead(200, {'Content-Type' : 'text/html'});
+// 		res.write(data);
+// 		console.log('Success');
+// 		return res.end();
+// 	});
+// }).listen(5000, () => console.log('Yes?'));
 
 // createServer(function (req, res) {
 //   var q = parse(req.url, true);
