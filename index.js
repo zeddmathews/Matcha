370a3887cc889cbf5404194097270253d2a658dd
Zain MATHEWS <zmathews@c5r11s5.wethinkcode.co.za>
2020-01-07 18:28:09 +0200

    Well that was a waste of a day

--- index.js
+++ index.js
@@ -1,61 +1,12 @@
-// import { createServer } from 'http';
-// import { parse } from 'url';
-// import { readFile } from 'fs';
+const express = require('express')
+const app = express()
 
-const http = require('http');
-const path = require('path');
-// const url = require('url');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {		//req = request, res = response
-	console.log(req.url);
-	if (req.url === '/') {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write('<h1>Home</h1>');
-		res.end();
-	}
-	// res.write('Is online');
-	// res.end();
+app.get('/', function (req, res) {
+  res.send('Hello World')
 })
 
-const PORT = process.env.PORT || 8888;
+app.get('/here', function(req, res) {
+	res.send(req.query.hello);
+})
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-// http.createServer((req, res) => {
-// 	console.log('oi');
-// 	var q = url.parse(req.url, true);
-// 	var fileName = "." + q.pathname;
-// 	fs.readFile(fileName, (err, data) => {
-// 		if (err) {
-// 			res.writeHead(404, {'Content-Type': 'text/html'});
-// 			console.log('Fail');
-// 			return res.end("404 Not Found");
-// 		}
-// 		res.writeHead(200, {'Content-Type' : 'text/html'});
-// 		res.write(data);
-// 		console.log('Success');
-// 		return res.end();
-// 	});
-// }).listen(5000, () => console.log('Yes?'));
-
-// createServer(function (req, res) {
-//   var q = parse(req.url, true);
-//   var filename = "." + q.pathname;
-//   readFile(filename, function(err, data) {
-//     if (err) {
-//       res.writeHead(404, {'Content-Type': 'text/html'});
-//       return res.end("404 Not Found");
-//     }
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(data);
-//     return res.end();
-//   });
-// }).listen(5000);
-
-
-// const req_http = require('http');
-// req_http.createServer((req, res) => {
-// 	res.write('Online');
-// 	res.end();
-// }).listen(5000, () => console.log('Server running'))
+app.listen(8888);
