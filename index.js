38802269688b5c841aaded83dce83ac3e36ef06c
Zain MATHEWS <zmathews@c5r11s5.wethinkcode.co.za>
2020-01-15 17:52:36 +0200

    Got rid of mongoose because that's an ODM genius, mongoDB connection, start of redirection with switch statement

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoDB = require('mongodb');
 const app = express();
+const port = process.env.PORT || 8888;
 
-const port = 8888; //set environment variable
+const defaultRoute = '/';
 
+app.use(bodyParser.json());
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended : false }));
+
+const checkRoute = (route) => {
+	switch (route) {
+		case "/login":
+
+	}
+};
+
+const mongoURI = "mongodb+srv://overStupid:@isPass175@matchacluster-hrimb.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoDB.connect(mongoURI, (err, client) => {
+	if (err) {
+		console.log(`Error connecting to database`);
+		throw err;
+	}
+	else {
+		console.log(`Connected to the database at: ${mongoURI}`);
+		client.close();
+	}
+});
 // if (app.get('/')) {
 // 	(req, res) => {
 
@@ -11,12 +38,10 @@ const port = 8888; //set environment variable
 //	use app.get with a switch statement to redirect to actual pages or 404 on a fuckout
 
 app.get('/', (req, res) => {
-	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.send('Hello World');
 })
 
 app.get('/here', (req, res) => {
-	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.send(req.query.hello);
 })
 
