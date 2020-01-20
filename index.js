e6ab76c6abdb858404a3000f51e495e0a66a817f
Zain MATHEWS <zmathews@c5r11s5.wethinkcode.co.za>
2020-01-20 12:42:30 +0200

    Switched port methods

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const bodyParser = require("body-parser");
 const mongoDB = require("mongodb");
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8888;
+// const port = process.env.PORT || 8888;
+
+app.set('port', process.env.PORT || 8888);
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended : false }));
 
-app.listen(port, (err) => {
+app.listen(app.get('port'), (err) => {
 	if (err) {
 		throw err;
 	}
 	else {
-		console.log(`Server running on port ${port}`);
+		console.log(`Server running`);
 	}
 });
 
@@ -30,6 +32,10 @@ app.get('/profile', (req, res) => {
 app.get('/match', (req, res) => {
 	res.sendFile(path.join(__dirname+'/match.html'));
 });
+
+app.get('/404', (req, res) => {
+	res.sendFile(path.join(__dirname+'/404.html'));
+})
 
 const mongoURI = "mongodb+srv://overStupid:@isPass175@matchacluster-hrimb.mongodb.net/test?retryWrites=true&w=majority";
 mongoDB.connect(mongoURI, {
