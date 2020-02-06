c9c54e0dd48436f878035e89ec6fbf4be27f64b6
Zain MATHEWS <zmathews@c5r11s5.wethinkcode.co.za>
2020-02-06 07:52:04 +0200

    method detection

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template					# importing flask module an creating a flask web server from said module
+from flask import Flask, render_template, request, redirect, session, url_for
 
-app = Flask(__name__)										# creating am instance of the flask class with the name 'app'. __name__ means this file. this current file will represent the web app
+app = Flask(__name__)
 
-@app.route('/')												# represents the default page
-def index():													# when user goes to default page of website this function will proc
+@app.route('/')
+def index():
 	return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		return redirect(url_for('index'))
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-	return render_template('register.html')
-if __name__ == '__main__':									# when the script is run, the name '__main__' is assigned on execution
-	app.run(debug=True)										# runs the app. debug=true is for error checking
+	if request.method == 'GET':
+		return render_template('register.html')
+	else:
+		print ('ifhgihdfiuhg')
+		return redirect(url_for('index'))
+
+if __name__ == '__main__':
+	app.run(debug=True)
