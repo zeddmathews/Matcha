2ec4fc3b3e08d737cf57219d4f3f5f96da2ade45
Zain <zmathews@student.wethinkcode.co.za>
2020-04-20 22:37:53 +0200

    and so it begins. for the 3948689378673904th time

---
+++ dbc.js
@@ -0,0 +1,36 @@
+var mysql = require('mysql');
+
+let options = {
+	// connectionLimit: 100,
+	host: 'localhost', //might need to change to xammp url (192.168.64.2)
+	// port: 3306,
+	user: 'root',
+	password: '123456',
+	database: 'matcha',
+	// socketPath: '/var/run/mysqld/mysqld.sock',
+};
+
+let connection = mysql.createConnection(options);
+
+connection.connect((err) => {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		console.log(`Connected to database: ${options.database}`);
+	}
+});
+
+module.exports = {
+	mysql,
+	connection
+}
+
+// pool.getConnection((err, connection) => {
+// 	if (err) {
+// 		throw err
+// 	}
+// 	else {
+// 		console.log(`Finally`);
+// 	}
+// })
