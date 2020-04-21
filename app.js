f5bd07a9a6b4fb7752c1f8ab48b3f0bf3eb0a536
Zain <zmathews@student.wethinkcode.co.za>
2020-04-21 20:36:16 +0200

    added redirects for logged in and logged out respectively

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressSession = require('express-session');
 
 var db = require('./dbc').connection;
 
@@ -15,6 +16,25 @@ var chatRouter = require('./routes/chat');
 
 var app = express();
 
+const session = expressSession({
+	secret: 'much secret',
+})
+
+const loginRedirect = (req, res, next) => {
+	if (!req.session.userID) {
+		res.render('login', {loginStatus : 'logged_out'});
+	}
+	else {
+		next();
+	}
+};
+
+const loggedInRedirect = (req, res, next) => {
+	if (req.session.userID) {
+		res.render('index', {loginStatus : 'logged_in'});
+	}
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,13 +44,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session);
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/login', loginRouter);
-app.use('/signup', signupRouter);
-app.use('/profile', profileRouter);
-app.use('/chat', chatRouter);
+app.use('/users', loginRedirect, usersRouter);
+app.use('/login', loggedInRedirect, loginRouter);
+app.use('/signup', loggedInRedirect, signupRouter);
+app.use('/profile',loginRedirect, profileRouter);
+app.use('/chat', loginRedirect, chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
