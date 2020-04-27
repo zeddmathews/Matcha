4c29d91ba0396f89e9a4069a4200b41fdd1e214c
Zain <zmathews@student.wethinkcode.co.za>
2020-04-27 20:03:30 +0200

    error objects and errors array

--- routes/signup.js
+++ routes/signup.js
@@ -3,22 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('signup', { title: 'Signup', loginStatus: req.session.userID ? 'logged_in' : 'logged_out' });
+  res.render('signup', { title: 'Signup', loginStatus: req.session.userID ? 'logged_in' : 'logged_out', errors : [] });
 });
 
 router.post('/create', (req, res) => {
-	let name = req.body.name;
-	let surname = req.body.surname;
-	let username = req.body.username;
-	let email = req.body.email;
-	let password = req.body.password;
-	let confirmPassword = req.body.confirmPassword;
+	let name = req.body.name.trim().toLowerCase();
+	let surname = req.body.surname.trim().toLowerCase();
+	let username = req.body.username.trim();
+	let email = req.body.email.trim();
+	let password = req.body.password.trim();
+	let confirmPassword = req.body.confirmPassword.trim();
 
-	let nameRegex = /^[A-Za-z]+$/;
-	let surnameRegex = ``;
-	let usernameRegex = ``;
-	let emailRegex = ``;
-	let passwordRegex = ``;
+	let alphaRegex = /^[A-Za-z]+$/;
+	let alphaNumRegex = /^[0-9A-Za-z_.-]+$/;
+	let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 
 	let nameErrors = {
 		length : ``,
@@ -52,13 +51,13 @@ router.post('/create', (req, res) => {
 	};
 	// name field
 	if (name.length > 0) {
-		if (!name.match(nameRegex)) {
+		if (!name.match(alphaRegex)) {
 			nameErrors[`casing`] = `Only uppercase and lowercase characters allowed`;
 		}
 		if (name.length > 20) {
 			nameErrors[`length`] = `May not be more than 20 characters`;
 		}
-		else if (name.match(nameRegex) && name.length <= 20) {
+		else if (name.match(alphaRegex) && name.length <= 20) {
 			nameErrors[`noErrors`] = `Yes`;
 		}
 	}
@@ -67,14 +66,30 @@ router.post('/create', (req, res) => {
 	}
 	// surname field
 	if (surname.length > 0) {
-
+		if (!surname.match(alphaRegex)) {
+			surnameErrors[`casing`] = `Only uppercase and lowercase characters allowed`;
+		}
+		if (surname.length > 20) {
+			surnameErrors[`length`] = `May not be more than 20 characters`;
+		}
+		else if (surname.match(alphaRegex) && surname.length <= 20) {
+			surnameErrors[`noErrors`] = `Yes`;
+		}
 	}
 	else {
 		surnameErrors[`length`] = `This field cannot be blank`;
 	}
 	// username field
 	if (username.length > 0) {
-
+		if (!username.match(alphaNumRegex)) {
+			usernameErrors[`casing`] = `Allowed characters include: Uppercase and lowercase letters and numbers`;
+		}
+		if (username.length > 30) {
+			usernameErrors[`length`] = `May not be more than 30 characters`;
+		}
+		else if (username.match(alphaNumRegex) && username.length <= 30) {
+			usernameErrors[`noErrors`] = `Yes`;
+		}
 	}
 	else {
 		usernameErrors[`length`] = `This field cannot be blank`;
@@ -100,6 +115,14 @@ router.post('/create', (req, res) => {
 	else {
 		confirmPasswordErrors[`length`] = `This field cannot be blank`;
 	}
+	let errors = [
+		nameErrors,
+		surnameErrors,
+		usernameErrors,
+		emailErrors,
+		passwordErrors,
+		confirmPasswordErrors
+	];
 });
 
 module.exports = router;
