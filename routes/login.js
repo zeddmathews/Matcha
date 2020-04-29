ebe1766ca131bc4867c2d05e4ebc17411424abd7
Zain <zmathews@student.wethinkcode.co.za>
2020-04-29 22:16:41 +0200

    login is nearly done, just need to double check password validation and create session variable

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 var connection = require('../dbc').connection;
 
 /* GET home page. */
@@ -14,15 +15,22 @@ router.get('/', (req, res, next) => {
 router.post('/attempt', (req, res, next) => {
 	let alphaNumRegex = /^[0-9A-Za-z_.-]+$/;
 	let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	let usernameEmail = req.body.usernameEmail;
-	let password = req.body.password;
+	let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+	let usernameEmail = req.body.usernameEmail.trim();
+	let password = req.body.password.trim();
 
 	let usernameEmailErrors = {
 		fieldLength : ``,
 		casing : ``,
-		noErrors : ``,
+		noErrors : `No`,
 		dbErrors : ``
 	};
+	let passwordErrors = {
+		fieldLength : ``,
+		casing : ``,
+		noErrors : `No`,
+		dbErrors : ``
+	}
 
 	if (usernameEmail.length > 0) {
 		if (!usernameEmail.match(alphaNumRegex) && !usernameEmail.match(emailRegex)) {
@@ -30,28 +38,70 @@ router.post('/attempt', (req, res, next) => {
 		}
 		else {
 			if (usernameEmail.match(alphaNumRegex)) {
-
+				usernameEmailErrors.noErrors = `Yes`;
+			}
+			else if (usernameEmail.match(emailRegex)) {
+				usernameEmailErrors.noErrors = `Yes`;
 			}
 		}
 	}
 	else {
 		usernameEmailErrors.fieldLength = `This field cannot be blank`;
 	}
-
-	let dataCheck = [usernameEmail, usernameEmail];
-	let dataCheckQuery = `SELECT email, username FROM users WHERE email = ? OR username = ?`
-	connection.query(dataCheckQuery, dataCheck, (err, results) => {
-		if (err) {
-			throw err;
+	if (password.length > 0) {
+		if (!password.match(passwordRegex)) {
+			passwordErrors.casing = `Invalid password`;
 		}
-		else if (results.length === 0) {
-			console.log(`no hablo engles`);
+		if (password.length > 15) {
+			passwordErrors.fieldLength = `Character limit exceeded`;
 		}
-		else if (results.length > 0) {
-			console.log(`Your face`);
+		else if (password.match(passwordRegex) && password.length <= 15) {
+			passwordErrors.noErrors = `Yes`;
 		}
-		// console.log(fields);
-	});
+	}
+	else {
+		passwordErrors.fieldLength = `This field cannot be blank`;
+	}
+	if (usernameEmailErrors.noErrors === `No` || passwordErrors === `No`) {
+		console.log(`It done didn't worked`);
+		let errors = [
+			usernameEmailErrors,
+			passwordErrors
+		];
+		res.render('Login', {
+			title: 'Login',
+			loginStatus: req.session.userID ? 'logged_in' : 'logged_out',
+			errors: errors,
+			verify: ``
+		});
+	}
+	else if (usernameEmailErrors.noErrors === `Yes` && passwordErrors.noErrors === `Yes`) {
+		console.log(`It done worked`);
+		let saltRounds = 10;
+		let dataCheck = [usernameEmail, usernameEmail];
+		let dataCheckQuery = `SELECT email, username, password FROM users WHERE email = ? OR username = ?`
+		connection.query(dataCheckQuery, dataCheck, (err, results) => {
+			if (err) {
+				throw err;
+			}
+			else if (results.length === 0) {
+				console.log(`no hablo engles`);
+			}
+			else if (results.length > 0) {
+				console.log(results[0].password);
+				console.log(password);
+				console.log(`Your face`);
+				// let hashComp = bcrypt.hashSync(password, saltRounds);
+				if (bcrypt.compareSync(results[0].password, password)) {
+					console.log(`Found password mugglefucker`);
+				}
+				else {
+					console.log(`Wrong password bitch`);
+				}
+			}
+			// console.log(fields);
+		});
+	}
 });
 
 module.exports = router;
