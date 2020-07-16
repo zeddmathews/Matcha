f667f5f84bd5b3c2345cf99e4f0ad4ffe8359905
Sherry <50695343+yu-lin42@users.noreply.github.com>
2020-07-16 19:53:33 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+	res.render('index', {
+		title: 'Index' ,
+		loginStatus: req.session.userID ? 'logged_in' : 'logged_out'
+	});
+});
+
+module.exports = router;
