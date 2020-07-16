a82de5400b31cc3097d06410e0dd83a6576a0ea0
Anubis <anubis@Anubiss-iMac.local>
2020-07-17 00:37:55 +0200

    Just added colour to navbar

---
+++ public/scripts/menu.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
+
+    // Get the container element
+var aContainer = document.getElementById("myDIV");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+}
